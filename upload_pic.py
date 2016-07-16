59228dcda1b6c93665211d75b3ef7764920bd368
Amrit Bhat <amritbhat786@gmail.com>
2016-07-17 03:59:51 +0530

    latest release

--- upload_pic.py
+++ upload_pic.py
@@ -7,20 +7,25 @@ user=db.user
 img_id=sys.argv[1]
 lat=sys.argv[2]
 lng=sys.argv[3]
+flag=0
 #ele = user.find()
 #cnt=user.find().count()
 for ele in user.find():
-	#res=cmp_img_id.py
+	#res=cmp_img_id.py #(call a script checking for similarity of image) 
 	res=0
 	if res==0:
 		icount = int(ele['count']) + 1
 		result_id = str(ele['person_id'])
+		flag=1
 		user.update_one({"person_id":result_id},{'$set':{"lat":lat,"long":lng,
 		                "count": str(icount)}},upsert=True)
 		if icount >3:
 			user.update_one({"person_id":result_id},{'$set':{"status":"Pd"}},upsert=True)	                
 	else:
-		user.insert_one({"person_id":img_id,"lat":lat,"long":lng,"count":1,
+		continue
+
+if flag==0:
+	user.insert_one({"person_id":img_id,"lat":lat,"long":lng,"count":1,
 		                "status":"P"}) 
 		
 
